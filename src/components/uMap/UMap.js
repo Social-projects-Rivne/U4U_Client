9cf0126cd8120bc4f76d15bb692e102c576504f2
Hordynskyi V <work.morpi@gmail.com>
2019-10-03 18:38:19 +0300

    deleted slogan

--- src/components/uMap/UMap.js
+++ src/components/uMap/UMap.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import './UMap.scss';
-import MapSvg from "./mapContent/mapSvg"
+import React from "react";
+import "./UMap.scss";
+import MapSvg from "./mapContent/mapSvg";
 
 const UMap = () => {
-    return (
-        <main className="map" id="map">
-			<h1 className="map__slogan">
-				Make your trip more fun in Ukraine
-			</h1>
+  return (
+    <main className="map" id="map">
+      <MapSvg />
 
-			<MapSvg />
-		
-			<div className="description"></div>
-		</main>
-    )
+      <div className="description"></div>
+    </main>
+  );
 };
 
 export default UMap;
