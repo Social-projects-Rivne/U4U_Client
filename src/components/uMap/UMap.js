a5b2156bf584aa684cdee12d0b6e446ed6269b40
Viktor H <work.morpi@gmail.com>
2019-10-04 13:57:12 +0300

    some updates

--- src/components/uMap/UMap.js
+++ src/components/uMap/UMap.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import "./UMap.scss";
 import MapSvg from "./mapContent/mapSvg";
 
-const UMap = () => {
-  return (
-    <main className="map" id="map">
-      <MapSvg />
+export default class UMap extends Component {
+  state = {
+    active: false,
+    x: 0,
+    y: 0,
+    region: ''
+  }
 
-      <div className="description"></div>
-    </main>
-  );
-};
+  mouseMove = (e) => {
+    this.setState({
+      active: true,
+      x: e.clientX,
+      y: e.clientY,
+      region: e.target.id
+    })
+  }
 
-export default UMap;
+  mouseOut = () => {
+    this.setState({
+      active: !this.state.active,
+      x: 0,
+      y: 0
+    })
+  }
+
+  render() {
+    const { x, y } = this.state;
+    const style = {
+      top: y, left: x
+    }
+
+    const active = (this.state.active) ? 'active' : ''
+
+    const region = (this.state.region !== 'Kyiv_City' && this.state.region !== 'Sevastopol_City')
+      ? `${this.state.region} region.`
+      : this.state.region.replace("_", " ")   
+
+    return (
+      <main className="map" id="map">
+        <MapSvg
+          mouseMove={this.mouseMove}
+          mouseOut={this.mouseOut}
+        />
+
+        <div className={`description ${active}`} style={style}>
+          {region}
+        </div>
+      </main>
+    );
+  }
+}
--- src/components/uMap/mapContent/mapSvg/MapSvg.js
+++ src/components/uMap/mapContent/mapSvg/MapSvg.js
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import MapContainer from "./../mapContainer";
 import rivneRegion from "./../regions.json";
 
-const MapSvg = () => {
-	return (
-		<MapContainer>
-			{rivneRegion.map(region => {
-				return (
-					<path
-						{...region}
-						key={region.id}
-						onClick={()=>alert(`redirect to ${region.id}`)
-						}
-					/>
-				)
-			})}
-		</MapContainer>
-	);
-};
+export default class MapSvg extends Component {
+	state = {
+		hover: true,
+		cHover: ''
+	}
 
-export default MapSvg;
+	mouseEnter = (e) => {
+		this.setState({
+			hover: false,
+			cHover: e.target.id
+		})
+	}
+
+	mouseLeave = (e) => {
+		this.setState({
+			hover: true,
+			cHover: e.target.id
+		})
+	}
+
+	render() {
+		return (
+			<MapContainer>
+				{rivneRegion.map(region => {
+					let hoverEffect = '';
+					if (!this.state.hover) {
+						if (this.state.cHover === region.id) hoverEffect = ''
+						else hoverEffect = 'blur'
+					}
+
+					return (
+						<path
+							{...region}
+							key={region.id}
+							onClick={() => alert(`redirect to ${region.id}`)}
+							onMouseEnter={this.mouseEnter}
+							onMouseLeave={this.mouseLeave}
+							className={`${region.className} ${hoverEffect}`}
+							onMouseMove={this.props.mouseMove}
+							onMouseOut={this.props.mouseOut}
+						/>
+					)
+				})}
+			</MapContainer>
+		);
+	}
+}
