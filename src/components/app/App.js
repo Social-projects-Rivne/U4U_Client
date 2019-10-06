25d01925fa08da35127e365b3cb6c8fbda282a0f
Dima Basiuk <dimabasyk993@gmail.com>
2019-10-06 18:21:44 +0300

    Fix merge conficts

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import './App.scss';
-import Header from '../header';
-import Container from '../container';
-import Footer from '../footer';
+import React, { useState } from 'react';
+import useAuth from '../hocs/useAuth';
+import './app.scss';
+
+import Login from "../pages/login/login";
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import { Notifications } from '../notify'
+
+import PrivateRoute from "../private-route";
 
 
-const App = () => {
+const Homepage = ({ history }) => {
+ return (
+     <ul>
+         <li onClick={() => history.push('/login')}>Login</li>
+         <li onClick={() => history.push('/secret')}>Secret</li>
+     </ul>
+ )
+};
+
+const App = initialState => {
+    const [state, setState] = useState({
+        isAuth: false,
+    });
+
+    const isAuth = useAuth();
+
+    const onAuth = (status) => {
+        setState({  ...state, isAuth: status })
+    };
+
     return (
-        <div className="wrapper">
-          <Header/>
-          <Container/>
-          <Footer/>
-        </div>
+        <>
+            <Router>
+                <Switch>
+                     <PrivateRoute path='/secret' onAuth={onAuth}
+                                   auth={state.isAuth}
+                                   Component={() => <h1>Secret Page</h1>} />
+
+                     <Route path='/' exact render={({history}) => {
+                         return <Homepage history={history} />
+                     }}/>
+                     <Route path='/login' render={() => {
+                        if(isAuth === null) return 'loading';
+                        if(isAuth) {
+                           return <Redirect to='/' />;
+                        }
+
+                       return <Login />
+                     }}/>
+                     <Route path='/pub2' render={() => <h1>Pub 2</h1>} />
+                </Switch>
+            </Router>
+
+            <Notifications />
+        </>
     )
 };
 
