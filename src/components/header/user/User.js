d23eec95349ad2ee53d40ecd241f2d6d8a429321
Viktor H <work.morpi@gmail.com>
2019-10-07 15:49:57 +0300

    Revert "added module"
    This reverts commit e5b1531c3c75a1cb11b0b0a63b8cdd7ad52e66e4.

--- src/components/header/user/User.js
+++ src/components/header/user/User.js
@@ -1,22 +1,8 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import './User.scss';
-import avatar from '../../../img/avatar.svg';
 
 const User = () => {
   return (
-    <div className="header__user">
-      <div className="header__user-avatar">
-        <img src={avatar} alt="user avatar" />
-			</div>
-
-      <div className="header__user-data">
-        <h5>Welcome, User</h5>
-        <Link to="/my-profile" className="header__profile">Your profile</Link>
-        <br/>
-        <Link to="/sign-out">Sign Out</Link>
-      </div>
-    </div>
+    <h1></h1>
   )
 }
 
