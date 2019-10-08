09c4ec761ffac28b3c508cdfd474d2d265353998
Hordynskyi V <work.morpi@gmail.com>
2019-10-08 19:02:35 +0300

    changes function

--- src/components/uMap/UMap.js
+++ src/components/uMap/UMap.js
@@ -1,59 +1,56 @@
 import React, { Component } from "react";
-import classNames from 'classnames';
+import classNames from "classnames";
 import "./UMap.scss";
 import MapSvg from "./mapContent/mapSvg";
-import RegionsName from './mapContent/regionsName.json'
+import RegionsName from "./mapContent/regionsName.json";
 
 export default class UMap extends Component {
   state = {
     active: false,
     x: 0,
     y: 0,
-    region: ''
-  }
+    region: ""
+  };
 
-  mouseMove = (e) => {
+  mouseMove = e => {
     this.setState({
       active: true,
       x: e.clientX,
       y: e.clientY,
       region: e.target.id
-    })
-  }
+    });
+  };
 
   mouseOut = () => {
     this.setState({
       active: !this.state.active,
       x: 0,
       y: 0
-    })
-  }
+    });
+  };
 
   render() {
     const { x, y } = this.state;
-    const clientHeight = document.documentElement.clientHeight
+    const clientHeight = document.documentElement.clientHeight;
     const style = {
       top: y,
       left: x
-    }
+    };
 
     const active = classNames({
-      'description': true,
-      'activeTop': this.state.active && (this.state.y < clientHeight / 2),
-      'activeBot': this.state.active && (this.state.y > clientHeight / 2) 
-    })
+      description: true,
+      activeTop: this.state.active && this.state.y < clientHeight / 2,
+      activeBot: this.state.active && this.state.y > clientHeight / 2
+    });
 
-    let regionName = '';
-    RegionsName.map(name => {
-      if (name.id === this.state.region) regionName = name.regionName
-    })
+    const { regionName } =
+      RegionsName.find(name => {
+        return this.state.region === name.id;
+      }) || {};
 
     return (
       <div className="map" id="map">
-        <MapSvg
-          mouseMove={this.mouseMove}
-          mouseOut={this.mouseOut}
-        />
+        <MapSvg mouseMove={this.mouseMove} mouseOut={this.mouseOut} />
 
         <div className={active} style={style}>
           {regionName}
