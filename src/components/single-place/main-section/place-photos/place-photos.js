772d3d5eac6b251ff5969de1a09051d0d57a5177
Dmitry Chekurkov <chekurkov@gmail.com>
2019-10-12 17:57:33 +0300

    In this commit i create basic logic for slider

--- src/components/single-place/main-section/place-photos/place-photos.js
+++ src/components/single-place/main-section/place-photos/place-photos.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './place-photos.scss';
 
-const PlacePhotos = ({photos}) =>{
-    console.log(photos);
-      return  (
-          <div className ='place-photo'>
-            <img src = {photos[1]}
-            alt = ''/>
-            </div>
-        )
+export default class PlacePhotos extends Component {
+
+    state={
+        currentPhoto: 0
     }
 
-export default PlacePhotos;
+    prev = () => {
+        const {currentPhoto} = this.state;
+        const {photos} = this.props;
 
+        this.setState({
+            currentPhoto: 1 - currentPhoto
+        })
+
+        if(currentPhoto < 0){
+            this.setState({
+                currentPhoto: photos.length - 1
+            })
+        }
+    }
+    
+    next = () => {
+        const {currentPhoto} = this.state;
+        const {photos} = this.props;
+        
+        this.setState({
+            currentPhoto: 1 + currentPhoto
+        })
+
+        if(currentPhoto >= photos.length - 1){
+            this.setState({
+                currentPhoto: 0
+            })
+        }
+    }
+
+    render(){
+        const {photos} = this.props;
+        const {currentPhoto} = this.state;
+
+        return(
+            <div className ='place-photo'>
+                <button onClick={this.prev}>Back</button>
+                <img src = {photos[currentPhoto]} alt = ''/>
+                <button onClick={this.next}>Next</button>
+            </div>
+        );
+    };
+};
