c2b7b16729d21825ff2ea285d8f0aaaa5d060ff7
Dmitry Chekurkov <chekurkov@gmail.com>
2019-10-13 18:31:36 +0300

    In this commit i try rewrite new logic for carusel

--- src/components/single-place/main-section/place-photos/place-photos.js
+++ src/components/single-place/main-section/place-photos/place-photos.js
@@ -6,53 +6,24 @@ import { faArrowAltCircleLeft, faArrowAltCircleRight } from '@fortawesome/free-s
 export default class PlacePhotos extends Component {
 
     state={
-        currentPhoto: 1
+       
     }
 
     prev = () => {
-        const {currentPhoto} = this.state;
-        const {photos} = this.props;
-
-
-        this.setState(({currentPhoto}) => {
-            console.log('set state on', currentPhoto)
-            return{
-                currentPhoto: --currentPhoto
-            }
-        })
-
-        if(currentPhoto <= 0){
-            this.setState({
-                currentPhoto: photos.length - 1
-            })
-        }
+        
     }
     
     next = () => {
-        const {currentPhoto} = this.state;
-        const {photos} = this.props;
-
-        this.setState(({currentPhoto}) => {
-            return{
-                currentPhoto: ++currentPhoto
-            }
-        })
-
-        if(currentPhoto >= photos.length - 1){
-            this.setState({
-                currentPhoto: 0
-            })
-        }
+       
     }
 
     render(){
         const {photos} = this.props;
-        const {currentPhoto} = this.state;
 
         return(
             <div className ='place-photo'>
                 <FontAwesomeIcon className="prev" icon={faArrowAltCircleLeft} onClick={this.prev} />
-                <img src = {photos[currentPhoto]} alt = ''/>
+                <img src = {photos[0]} alt = ''/>
                 <FontAwesomeIcon className="next" icon={faArrowAltCircleRight} onClick={this.next} />
             </div>
         );
