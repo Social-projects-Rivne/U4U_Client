09f02b7db70f8cac00419b2390fa094a36692e42
Dmitry Chekurkov <chekurkov@gmail.com>
2019-10-14 15:42:18 +0300

    In this commit i added animation to our slider

--- src/components/single-place/main-section/place-photos/place-photos.js
+++ src/components/single-place/main-section/place-photos/place-photos.js
@@ -4,24 +4,39 @@ import './place-photos.scss';
 export default class PlacePhotos extends Component {
 
     state={
-        currentSlide: 0
+        currentSlide: 0,
+        animation: false,
     }
 
     prev = () => {
         const {photos} = this.props;
         const {currentSlide} = this.state;
+
+        this.setState(({animation}) => {
+            return{
+                animation: !animation
+            }
+        });
         
         if(currentSlide <= 0){
-            this.setState({
-                currentSlide: photos.length - 1
-            })
+            setTimeout(() => {
+                this.setState(({animation, currentSlide}) => {
+                    return{
+                        animation: !animation,
+                        currentSlide: photos.length - 1
+                    }
+                })
+            }, 500);
         }
         else{
-            this.setState(({currentSlide}) => {
+           setTimeout(() => {
+            this.setState(({currentSlide, animation}) => {
                 return{
+                    animation: !animation,
                     currentSlide: --currentSlide
                 }
             })
+           }, 500);
         }
     }
     
@@ -29,30 +44,46 @@ export default class PlacePhotos extends Component {
         const {photos} = this.props;
         const {currentSlide} = this.state;
 
+        this.setState(({animation}) => {
+            return{
+                animation: !animation
+            }
+        });
+
         if(currentSlide >= photos.length - 1){
-            this.setState({
-                currentSlide: 0
-            })
+            setTimeout(() => {
+                this.setState(({currentSlide, animation}) => {
+                    return{
+                        animation: !animation,
+                        currentSlide: 0
+                    }
+                })
+            }, 500);
         }
         else{
-            this.setState(({currentSlide}) => {
-                return{
-                    currentSlide: ++currentSlide
-                }
-            })
+            setTimeout(() => {
+                this.setState(({currentSlide, animation}) => {
+                    return{
+                        animation: !animation,
+                        currentSlide: ++currentSlide
+                    }
+                })
+            }, 500);
         }
     }
 
     
     render(){
         const {photos} = this.props;
-        const {currentSlide} = this.state;
+        const {currentSlide, animation} = this.state;
+
+        const animationClass = animation ? 'sliderAnimation' : 'sliderAppend';
         return(
             <div className ='place-photo'>
                 <div className="prev" onClick={this.prev}>
                     &laquo;
                 </div>
-                <img src = {photos[currentSlide]} alt = ''/>
+                <img src = {photos[currentSlide]} alt ='' className={animationClass}/>
                 <div className="next" onClick={this.next}>
                     &raquo;
                 </div>
