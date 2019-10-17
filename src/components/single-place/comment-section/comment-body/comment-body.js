65fc020c458db892c77a5c73dd6be284aeaac0c7
Dmitry Chekurkov <chekurkov@gmail.com>
2019-10-17 15:20:48 +0300

    In this commit i create pretty UI for post-form

--- src/components/single-place/comment-section/comment-body/comment-body.js
+++ src/components/single-place/comment-section/comment-body/comment-body.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import './comment-body.scss';
 
 export default class CommentBody extends Component {
-    render() {
-
-        return (
-            <div className='comment-body'>
-                <p className='comment-sharing'>Share your advantures...</p>
-                <form action="" method="post">
-                    <textarea name='comment'></textarea><br></br>
-                    <input type='submit' value='comment' id='comment-button' ></input>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='comment-body'>
+        <p className='comment-sharing'>Share your advantures about this place...</p>
+        <form action="" method="post">
+          <textarea name='comment' placeholder='Tell something about this place, please :)'></textarea>
+          <input type='submit' value='Comment' id='comment-button' ></input>
+        </form>
+      </div>
+    )
+  }
 }
