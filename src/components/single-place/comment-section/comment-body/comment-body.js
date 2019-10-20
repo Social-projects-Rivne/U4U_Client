8f19512e9303d2fe8710fb720a61438d3068fad9
Dmitry Chekurkov <chekurkov@gmail.com>
2019-10-20 21:49:34 +0300

    In this commit i created small validation to comment

--- src/components/single-place/comment-section/comment-body/comment-body.js
+++ src/components/single-place/comment-section/comment-body/comment-body.js
@@ -2,45 +2,50 @@ import React, { Component } from 'react';
 import './comment-body.scss';
 import api from '../../../../services/review-service';
 
-
 export default class CommentBody extends Component {
 
   state = {
     comment: null,
-    rating: null,
+    commentError: false,
   }
 
-  onSubmith = (e) => {
+  onSubmit = (e) => {
     e.preventDefault();
-    const {comment, rating} = this.state;
-    const jwt = localStorage.getItem('token');
+    const {comment} = this.state;
+    if(comment.length <= 2){
+      this.setState({commentError: true})
+    }
+    else{
+      this.setState({commentError: false})
 
-    // place Id is hardcode becouse we have not place ID from main page, Rating is hardCode becouse we havenot rating stars component
-    api.comment({userJwt: jwt, comment: comment, placeId:'5d8f77580b43e2050ca9d43f', rating: 5}) // --- hardcode
-    .then(() => {
-      console.log('Success');
-    })
-    .catch((err) => {
-      this.setState(({errorComment: err}));
-    })
+      const jwt = localStorage.getItem('token');
+
+      //place Id is hardcode becouse we have not place ID from main page, Rating is hardCode becouse we havenot rating stars component
+      api.comment({userJwt: jwt, comment: comment, placeId:'5d8f77580b43e2050ca9d43f', rating: 5}) // --- hardcode
+      .then(() => {
+          console.log('Success');
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }
   }
 
-  onCommentChange = (e) => {
-    this.setState({ comment: e.target.value })
-  };
-
-  onRatingChange = (e) => {
-    this.setState({ rating: 5 }) // --- hardcode
+  onComment = (e) => {
+    this.setState({comment: e.target.value});
   }
 
   render() {
+    const {commentError} = this.state;
+
+    const error = commentError ? 'Comment length must be bigger then 2' : '';
     return (
       <div className='comment-body'>
         <p className='comment-sharing'>Share your advantures about this place...</p>
-        <form onSubmit={this.onSubmith}>
-          <textarea name='comment' placeholder='Tell something about this place, please :)' onChange={this.onCommentChange}></textarea>
-          <p className='message'>{}</p>
-          <input type='submit' value='Comment' id='comment-button'></input>
+        <form onSubmit={this.onSubmit}>
+          <textarea name='comment' placeholder='Tell something about this place, please :)' className='comment' onChange={this.onComment}></textarea>
+          <p className='message'>{error}</p>
+          <input type='submit' id='comment-button' value='Send'></input> 
         </form>
       </div>
     )
