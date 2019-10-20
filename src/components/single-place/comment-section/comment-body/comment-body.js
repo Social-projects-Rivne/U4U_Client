efee77b98b3eb00960c834969acd29587fc9d17e
Dmitry Chekurkov <chekurkov@gmail.com>
2019-10-20 19:04:01 +0300

    In this commit i create configure for post request

--- src/components/single-place/comment-section/comment-body/comment-body.js
+++ src/components/single-place/comment-section/comment-body/comment-body.js
@@ -1,31 +1,46 @@
 import React, { Component } from 'react';
 import './comment-body.scss';
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
 import api from '../../../../services/review-service';
-import {Redirect} from "react-router-dom";
+
 
 export default class CommentBody extends Component {
 
+  state = {
+    comment: null,
+    rating: null,
+  }
+
   onSubmith = (e) => {
     e.preventDefault();
-    api.comment({userJwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTU3MTQwMzg4NCwiZXhwIjoxNTcxNDE1ODg0fQ.rkqW8sTDvZQFIenem7EjIo_FUaepGLcmol1FNvXdrgw',comment: 'Cool place', placeId:'Example', rating: '2'})
-    .then((res) => {
-      console.log(res)
+    const {comment, rating} = this.state;
+    const jwt = localStorage.getItem('token');
+
+    // place Id is hardcode becouse we have not place ID from main page, Rating is hardCode becouse we havenot rating stars component
+    api.comment({userJwt: jwt, comment: comment, placeId:'5d8f77580b43e2050ca9d43f', rating: 5}) // --- hardcode
+    .then(() => {
+      console.log('Success');
     })
     .catch((err) => {
-      console.log(err);
+      this.setState(({errorComment: err}));
     })
   }
 
-  render() {
+  onCommentChange = (e) => {
+    this.setState({ comment: e.target.value })
+  };
 
+  onRatingChange = (e) => {
+    this.setState({ rating: 5 }) // --- hardcode
+  }
+
+  render() {
     return (
       <div className='comment-body'>
         <p className='comment-sharing'>Share your advantures about this place...</p>
-        <form action="" method="post" onClick={this.onSubmith}>
-          <textarea name='comment' placeholder='Tell something about this place, please :)'></textarea>
-          <input type='submit' value='Comment' id='comment-button' ></input>
+        <form onSubmit={this.onSubmith}>
+          <textarea name='comment' placeholder='Tell something about this place, please :)' onChange={this.onCommentChange}></textarea>
+          <p className='message'>{}</p>
+          <input type='submit' value='Comment' id='comment-button'></input>
         </form>
       </div>
     )
