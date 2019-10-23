8e244c80f244de2e6a9005db40ba557ec0534ac1
Dmitry Chekurkov <chekurkov@gmail.com>
2019-10-23 20:37:54 +0300

    In this commit i created rating component

--- src/components/single-place/comment-section/comment-body/comment-body.js
+++ src/components/single-place/comment-section/comment-body/comment-body.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './comment-body.scss';
 import api from '../../../../services/review-service';
+import Rating from '../../../utils/rating-stars';
 
 export default class CommentBody extends Component {
 
   state = {
     comment: null,
     commentError: false,
+    mark: null
+  }
+
+  mark = (value) => {
+    this.setState({ mark: value });
   }
 
   onSubmit = (e) => {
@@ -19,9 +25,10 @@ export default class CommentBody extends Component {
       this.setState({commentError: false})
 
       const jwt = localStorage.getItem('token');
+      const { mark } = this.state;
 
       //place Id is hardcode becouse we have not place ID from main page, Rating is hardCode becouse we havenot rating stars component
-      api.comment({userJwt: jwt, comment: comment, placeId:'5d8f77580b43e2050ca9d43f', rating: 5}) // --- hardcode
+      api.comment({userJwt: jwt, comment: comment, placeId:'5d8f77580b43e2050ca9d43f', rating: mark}) // --- hardcode
       .then(() => {
           console.log('Success');
       })
@@ -45,6 +52,13 @@ export default class CommentBody extends Component {
         <form onSubmit={this.onSubmit}>
           <textarea name='comment' placeholder='Tell something about this place, please :)' className='comment' onChange={this.onComment}></textarea>
           <p className='message'>{error}</p>
+          <div className='rating-container'>
+            <Rating value={1} onStar={this.mark} />
+            <Rating value={2} onStar={this.mark} />
+            <Rating value={3} onStar={this.mark} />
+            <Rating value={4} onStar={this.mark}/>
+            <Rating value={5} onStar={this.mark}/>
+          </div>
           <input type='submit' id='comment-button' value='Send'></input> 
         </form>
       </div>
