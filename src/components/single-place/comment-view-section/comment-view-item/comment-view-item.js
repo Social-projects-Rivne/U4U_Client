9daeddf1dd185c1d2060f87df57d005bc8f40ae5
andrii-kondratiuk <kondratyuk.andy@gmail.com>
2019-11-05 12:29:31 +0200

    comments css changes

--- src/components/single-place/comment-view-section/comment-view-item/comment-view-item.js
+++ src/components/single-place/comment-view-section/comment-view-item/comment-view-item.js
@@ -4,12 +4,12 @@ import './comment-view-item.scss';
 
 export default class CommentViewItem extends Component {
   render() {
-    const { userId, rating, createdBy, comment } = this.props;
+    const { userId, rating, createdAt, comment } = this.props;
     return (
       <div className="grid-container">
         <div className="user">{userId}</div>
         <div className="rating">{rating}</div>
-        <div className="date"> {createdBy} </div>
+        <div className="date"> {createdAt} </div>
         <div className="body">{comment}</div>
       </div>
     );
