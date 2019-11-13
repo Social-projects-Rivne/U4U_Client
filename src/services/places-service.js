9130ced0484a65cf7e91391793f28848036b11ce
Viktor Poliushko <milleniiumkb@gmail.com>
2019-11-14 00:28:13 +0200

    redirect from map

--- src/services/places-service.js
+++ src/services/places-service.js
@@ -1,36 +1,39 @@
-import Request from './request';
-const PLACES_URL = 'regions/districts/places';
-const SEARCH_PARAM_URL = 'places/search';
+import Request from "./request";
+const PLACES_URL = "regions/districts/places";
+const SEARCH_PARAM_URL = "places/search";
 
 class Api {
-    getAllPlaces = async () => {
-        try {
-            return await Request.get(PLACES_URL);
-        } catch (error) {
-            throw new Error(error.message);
-        }
-    };
-
-    getPlace = async (id) => {
-        try {
-            return await Request.get(`${PLACES_URL}/${id}`);
-        } catch (error) {
-            throw new Error(error.message)
-        }
+  getAllPlaces = async () => {
+    try {
+      return await Request.get(PLACES_URL);
+    } catch (error) {
+      throw new Error(error.message);
     }
+  };
 
-    getSearchParameters = async (searchInput) => {
-        try {
-            return await Request.get(`${SEARCH_PARAM_URL}/${searchInput}`)
-            
-        } catch (error) {
-            throw new Error(error.message)
-        }
-             
-           
-
-    
+  getRegionPlaces = async id => {
+    try {
+      return await Request.get(`regions/${id}/places`);
+    } catch (error) {
+      throw new Error(error.message);
     }
+  };
+
+  getPlace = async id => {
+    try {
+      return await Request.get(`${PLACES_URL}/${id}`);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  getSearchParameters = async searchInput => {
+    try {
+      return await Request.get(`${SEARCH_PARAM_URL}/${searchInput}`);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
 }
 
 export default new Api();
