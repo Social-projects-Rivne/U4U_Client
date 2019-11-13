ca6b5d792b6b7480b68737a2d84d5f8e851525d9
Dmitry Chekurkov <chekurkov@gmail.com>
2019-11-13 16:19:14 +0200

    In this commit i add user email to comments

--- src/components/single-place/comment-view-section/comment-view-item/comment-view-item.js
+++ src/components/single-place/comment-view-section/comment-view-item/comment-view-item.js
@@ -3,10 +3,10 @@ import './comment-view-item.scss';
 
 export default class CommentViewItem extends Component {
   render() {
-    const { userId, rating, createdAt, comment } = this.props;
+    const { userEmail, rating, createdAt, comment } = this.props;
     return (
       <div className="grid-container">
-        <div className="user">{userId}</div>
+        <div className="user">{userEmail}</div>
         <div className="rating">{rating}☆</div>
         <div className="date"> {createdAt}</div>
         <div className="body">{comment}</div>
