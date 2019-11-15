48a9919c0f544131f54cc83ecb59e6fc1d1dee9f
Dmitry Chekurkov <chekurkov@gmail.com>
2019-11-15 15:46:14 +0200

    In this commit i added avatars and userNames

--- src/components/single-place/comment-view-section/comment-view-item/comment-view-item.js
+++ src/components/single-place/comment-view-section/comment-view-item/comment-view-item.js
@@ -3,13 +3,15 @@ import './comment-view-item.scss';
 
 export default class CommentViewItem extends Component {
   render() {
-    const { userEmail, rating, createdAt, comment } = this.props;
+    const { userEmail, rating, createdAt, comment, userNickname, userAvatar } = this.props;
     return (
       <div className="grid-container">
         <div className="user">{userEmail}</div>
         <div className="rating">{rating}☆</div>
         <div className="date"> {createdAt}</div>
         <div className="body">{comment}</div>
+        <div className="nickname">{userNickname}</div>
+        <div className="avatar"><img src={userAvatar}  alt="avatar"/></div>
       </div>
     );
   }
