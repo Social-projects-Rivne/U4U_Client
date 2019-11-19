0f36736f85e3ebc5f09ae87bfad67504b6ded018
Max Inno <maksym.hubach@gmail.com>
2019-11-20 00:55:05 +0200

    basic implementation (changes don't display immediately)

--- src/components/profile/user-info-section/user-info-form/user-info-form.js
+++ src/components/profile/user-info-section/user-info-form/user-info-form.js
@@ -1,28 +1,103 @@
 import React, { Component } from "react";
+import UserService from "../../../../services/user-service"
 
 import "./user-info-form.scss";
 
 export default class UserInfoForm extends Component {
+  user = {};
+
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      name: this.props.user.name,
+      surname: this.props.user.surname,
+      nickname: this.props.user.nickname,
+      email: this.props.user.email
+    };
     if (!props.user) {
       console.log(props.user);
     }
   }
+
+  editUserData = (body) => {
+    UserService.editUserData(body).then(user => {
+      this.setState({
+        name: user.name,
+        surname: user.surname,
+        nickname: user.nickname,
+        email: user.email
+      })
+    }).catch(console.error)
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+
+    const profileData = [
+      { id: "name", value: this.state.newUserName },
+      { id: "surname", value: this.state.newSurName },
+      { id: "nickname", value: this.state.newNickName },
+      { id: "email", value: this.state.newEmail }
+    ];
+
+    const body = new Object()
+    for (const field of profileData) {
+      if (!!field.value) {
+        body[field.id] = field.value
+      }
+    }
+    console.log(body);
+    this.editUserData(body);
+  };
+
+  handleUserName = (event) => {
+    this.setState({
+      newUserName: event.target.value
+    })
+  };
+
+  handleSurName = (event) => {
+    this.setState({
+      newSurName: event.target.value
+    })
+  };
+
+  handleNickName = (event) => {
+    this.setState({
+      newNickName: event.target.value
+    })
+  };
+
+  handleEmail = (event) => {
+    this.setState({
+      newEmail: event.target.value
+    })
+  };
+
   render() {
     if (this.props.user) {
       return (
-        <form className="user-info-form" action="">
+        <form className="user-info-form" onSubmit={this.handleSubmit}>
           <div className="user-info-center">
             <div className="user-info_center-group">
               <span>Name</span>
-              <input className="user-info-name" type="text" name="name" value={this.props.user.name}/>
+              <input
+                className="user-info-name"
+                type="text"
+                name="name"
+                onChange={this.handleUserName}
+                defaultValue={this.state.name} />
             </div>
             <div className="user-info_center-group">
               <span>Surname</span>
-              <input className="user-info-surname" type="text" name="surname" value={this.props.user.surname}/>
+              <input
+                  className="user-info-surname"
+                  type="text"
+                  name="surname"
+                  onChange={this.handleSurName}
+                  defaultValue={this.state.surname} />
             </div>
             <div className="user-info_center-group">
               <span>Nickname</span>
@@ -30,12 +105,17 @@ export default class UserInfoForm extends Component {
                 className="user-info-nickname"
                 type="text"
                 name="nickname"
-                value={this.props.user.nickname}
-              />
+                onChange={this.handleNickName}
+                defaultValue={this.state.nickname} />
             </div>
             <div className="user-info_center-group">
               <span>Email</span>
-              <input className="user-info-email" type="text" name="email" value={this.props.user.email}/>
+              <input
+                  className="user-info-email"
+                  type="text"
+                  name="email"
+                  onChange={this.handleEmail}
+                  defaultValue={this.state.email} />
             </div>
           </div>
           <div className="user-info-right">
