fe6dbb23a4a94ef844c3177794863e7b9245437d
Viktor <work.morpi@gmail.com>
2019-11-25 09:55:06 +0200

    new view of wish button

--- src/components/single-place/main-section/add-to-wish-list-bttn/add-to-wish-list-bttn.js
+++ src/components/single-place/main-section/add-to-wish-list-bttn/add-to-wish-list-bttn.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import './add-to-wish-list-bttn.scss';
 
 export default class AddToWishListBttn extends Component {
-   render() {
-      return(
-        <div className ='add-to-wish-list-bttn'>
-            <FontAwesomeIcon icon={faPlus}
-             size="2x"
-             title='Add place to my Wish List'
-             />
-         </div>
-      )
-   }
+  render() {
+    return (
+      <div className='add-to-wish-list-bttn'>
+        <label class="wish-checkbox">
+          Wish to Visit <input type="checkbox" />
+          <span class="wish-checkmark"></span>
+        </label>
+      </div>
+    )
+  }
 }
