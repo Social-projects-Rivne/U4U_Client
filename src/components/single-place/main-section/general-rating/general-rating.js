0e3d09769fb120402c46a2a7bffc64af90b99f3c
Viktor <work.morpi@gmail.com>
2019-11-25 09:52:43 +0200

    new view: avg rating

--- src/components/single-place/main-section/general-rating/general-rating.js
+++ src/components/single-place/main-section/general-rating/general-rating.js
@@ -4,45 +4,13 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 import './general-rating.scss';
 
 export default class GeneralRating extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      hover: false,
-      starsSelected: 0
-    };
-  };
-
-  handleStarHover = () => {
-    this.setState({
-      hover: true,
-
-    });
-  };
-
-  handleStarBlur = () => {
-    this.setState({
-      hover: false
-    });
-  };
-
-  onStarClick = () => {
-
-  };
   render() {
-    const starsCount = [1, 2, 3, 4, 5];
+    const { ratingAvg } = this.props
     return (
       <div className='all-stars-container' >
-        {starsCount.map(index => (
-          <div key={index} className='star-container'
-            style={{ color: this.state.hover ? 'yellow' : 'white' }}
-            onMouseEnter={this.handleStarHover}
-            onMouseLeave={this.handleStarBlur}>
-            <FontAwesomeIcon icon={faStar} size="2x" />
-          </div>
-        )
-        )}
+        <span>{ratingAvg}</span>
+        <FontAwesomeIcon icon={faStar} />
       </div>
     )
   }
 }
-
