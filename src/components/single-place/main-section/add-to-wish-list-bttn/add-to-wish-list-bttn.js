fbd193ac71b0dc2d8a57c3e828281e9cf31b9ecb
Viktor <work.morpi@gmail.com>
2019-11-29 23:14:39 +0200

    refactoring, small fix and clearing

--- src/components/single-place/main-section/add-to-wish-list-bttn/add-to-wish-list-bttn.js
+++ src/components/single-place/main-section/add-to-wish-list-bttn/add-to-wish-list-bttn.js
@@ -14,65 +14,47 @@ export default class AddToWishListBttn extends Component {
   componentDidMount() {
     this.service.getPlansList()
       .then((plansList) => {
-        const currentUserListArr = []
-
-        plansList.filter(e => {
-          if(+e.userId === +this.props.loggedInUserId) {
-            return currentUserListArr.push(e)
-          }
-        })
-  
-        const find = currentUserListArr.find(place => {
+        const find = plansList.find(place => {
           return place.placeId === this.props.currentPlaceId
         })
-        if(find) {
+        if (find) {
           this.setState({
             myPlansList: plansList,
             wishState: true,
             wishId: find._id
           })
-        }        
+        }
       }).catch((error) => {
         console.log(error)
       })
   }
 
-  deleteItem = (id) => {
-    this.service.deleteWish(id).then((res) => {
-      console.log(res)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  }
-
   toWish = (wishId, placeId, placeName) => {
-    // console.log(wishId)
-    // console.log(placeId)
-    // console.log(placeName)
-    if(!this.state.wishState) {
+    if (!this.state.wishState) {
       const newPlace = {
         placeId,
         placeName,
         done: false,
         inProgress: false
       }
-      this.service.postWish(newPlace).then((res) => {})
-      .catch((error) => {
-        console.log(error)
+      this.service.postWish(newPlace).then((res) => {
+        this.setState({
+          wishState: true
+        })
       })
+        .catch((error) => {
+          console.log(error)
+        })
     } else {
-      this.service.deleteWish(wishId).then((res) => {})
-      .catch((error) => {
-        console.log(error)
+      this.service.deleteWish(wishId).then((res) => {
+        this.setState({
+          wishState: false
+        })
       })
+        .catch((error) => {
+          console.log(error)
+        })
     }
-
-
-
-    this.setState({
-      wishState: !this.state.wishState
-    })
   }
 
   render() {
@@ -82,10 +64,14 @@ export default class AddToWishListBttn extends Component {
     return (
       <div className='add-to-wish-list-bttn'>
         <label class={`wish-checkbox ${disabled}`}>
-          Wish to visit <input type="checkbox" checked={wishState?'checked':''} onClick={() => this.toWish(
-            this.state.wishId, 
-            this.props.currentPlaceId,
-            this.props.currentPlaceName)} />
+          Wish to visit
+          <input
+            type="checkbox"
+            checked={wishState ? 'checked' : ''}
+            onClick={() => this.toWish(
+              this.state.wishId,
+              this.props.currentPlaceId,
+              this.props.currentPlaceName)} />
           <span class="wish-checkmark"></span>
         </label>
       </div>
